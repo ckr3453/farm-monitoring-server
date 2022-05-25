7f6469512d39a7246c2d52fb1e1a0a1f523cf3fe
주명길 <jumyeonggil@jumyeong-gil-ui-MacBookAir.local>
2022-05-25 23:27:00 +0900

    [FIX] data type

--- queries.py
+++ queries.py
@@ -25,10 +25,10 @@ def create(db: Session, data: dict):
 def update(db: Session, id: int, data: dict):
     print(data['start_time'][:10])
     try:
-        row = {'id': data['id'], 'value1': data['value1'], 'value2': data['value2'], 'start_time': data['start_time'][:10], 'in_time': data['in_time'][:10]}
+        row = {'id': int(data['id']), 'value1': int(data['value1']), 'value2': data['value2'], 'start_time': data['start_time'][:10], 'in_time': data['in_time'][:10]}
         db.query(models.Geobong).filter(models.Geobong.id == id).update(row)
         db.commit()
-        data_instance = models.Geobong(id=data['id'], value1=data['value1'], value2=data['value2'], start_time=data['start_time'][:10], in_time=data['in_time'][:10])
+        data_instance = models.Geobong(id=int(data['id']), value1=int(data['value1']), value2=data['value2'], start_time=data['start_time'][:10], in_time=data['in_time'][:10])
         db.refresh(data_instance)
     except Exception as err:
         data_instance = {}
