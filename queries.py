c9993a1a392a8c21cbf4ff053af49884833e3996
주명길 <jumyeonggil@jumyeong-gil-ui-MacBookAir.local>
2022-05-25 23:22:49 +0900

    [FIX] data instance at update

--- queries.py
+++ queries.py
@@ -25,12 +25,13 @@ def create(db: Session, data: dict):
 def update(db: Session, id: int, data: dict):
     print(data['start_time'][:10])
     try:
-        # data_instance = models.Geobong(id=data['id'], value1=data['value1'], value2=data['value2'], start_time=data['start_time'][:10], in_time=data['in_time'][:10])
         row = {'id': data['id'], 'value1': data['value1'], 'value2': data['value2'], 'start_time': data['start_time'][:10], 'in_time': data['in_time'][:10]}
         db.query(models.Geobong).filter(models.Geobong.id == id).update(row)
         db.commit()
+        data_instance = models.Geobong(id=data['id'], value1=data['value1'], value2=data['value2'], start_time=data['start_time'][:10], in_time=data['in_time'][:10])
         db.refresh(data_instance)
     except Exception as err:
+        data_instance = {}
         print(err)
     return data_instance
 
